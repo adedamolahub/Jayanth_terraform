d290b5396007555ae7b9427801e379bc579186dc
Adedamola <adedamolamalik@yahoo.com>
2023-10-31 11:00:51 +0000

    Update code.java

--- code.java
+++ code.java
@@ -4,4 +4,8 @@ int age = 67;
 public void giveInfo(name, age){
     System.out.println("His name is " + name + " and his age is " + age);
     System.out.println("this is great!");
+    System.out.println("this is great!");
+    System.out.println("this is great!");
+    System.out.println("this is great!");
+    System.out.println("this is great!");
 };
