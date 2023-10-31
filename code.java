0296240fdfa635288a3aa66e352e5b60de256122
adedamolahub <adedamolamalik@yahoo.com>
2023-10-31 12:30:57 +0000

    newest commit

--- code.java
+++ code.java
@@ -5,4 +5,5 @@ public void giveInfo(name, age){
     System.out.println("His name is " + name + " and his age is " + age);
     System.out.println("this is great!");
     System.out.println("this is great!");
+    System.out.println("God is great!");
 };
